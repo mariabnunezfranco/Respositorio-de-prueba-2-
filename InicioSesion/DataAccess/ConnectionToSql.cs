cdce8bf09b9d60a04f0ca03046e7a784e01996cc
María Belén Núñez Franco <maria.b.nunez.franco@avanade.com>
2022-06-29 13:50:40 +0200

    Agregamos proyecto

---
+++ InicioSesion/DataAccess/ConnectionToSql.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+
+namespace DataAcces
+{
+    public abstract class ConnectionToSql
+    {
+
+
+        private string connectionString;
+
+        public ConnectionToSql()
+        {
+            connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Database = MyCompany;Integrated Security=True";
+            //connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Database = MyCompany;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        }
+
+        protected SqlConnection GetConnection()
+        {
+
+            return new SqlConnection(connectionString);
+
+        }
+
+        //cnn.Open();
+        //MessageBox.Show("Connection Open  !");
+        //cnn.Close();
+
+    }
+}
---
+++ InicioSesion/Presentación/Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace Presentación
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(525, 244);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.TextBox TextoUsuario;
+        private System.Windows.Forms.TextBox TextoContraseña;
+        private System.Windows.Forms.Button BotonInicioSesion;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label MensajeError;
+    }
+}
